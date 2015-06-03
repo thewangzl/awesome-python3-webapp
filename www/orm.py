620c37df46904b699c7d046fbb50f25fb59097c8
thewangzl <thewangzl@gmail.com>
2015-06-03 10:47:37 +0800

    fix bug

--- www/orm.py
+++ www/orm.py
@@ -2,17 +2,20 @@ import asyncio,logging
 
 import aiomysql
 
+def log(sql,args=()):
+	logging.info('SQL: %s' % sql)
+
 @asyncio.coroutine
 def create_pool(loop,**kw):
 	logging.info('create database connection pool...')
-	global _pool
-	_pool = yield from aiomysql.create_pool(
+	global __pool
+	__pool = yield from aiomysql.create_pool(
 		host = kw.get('host','localhost'),
 		port = 	kw.get('port',3306),
 		user = kw['user'],
 		password = kw['password'],
 		db = kw['db'],
-		charset = kw.get('charset','utf-8'),
+		charset = kw.get('charset','utf8'),
 		autocommit = kw.get('autocommit',True),
 		maxsize = kw.get('maxsize',10),
 		minsize = kw.get('minsize',1),
