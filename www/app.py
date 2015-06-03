6ca4aa36b78d0781b5c772f83688ebf81a2ba881
thewangzl <thewangzl@gmail.com>
2015-06-03 12:03:03 +0800

    fix bug

--- www/app.py
+++ www/app.py
@@ -61,28 +61,37 @@ def response_factory(app,handler):
 		logging.info('Response handler...')
 		r = yield from handler(request)
 		if isinstance(r,web.StreamResponse):
+			logging.info('web.StreamResponse.')
 			return r
 		if isinstance(r, bytes):
+			logging.info('byte.')
 			resp = web.Response(body=r)
 			resp.content_type = 'application/octet-stream'
 			return resp
 		if isinstance(r, str):
+			logging.info('str.')
 			if r.startswith('redirect:'):
 				return web.HTTPFound(r[9:])
 			resp = web.Response(body = r.encode('utf-8'))
 			resp.content_type = 'text/html;charset=utf-8'
 			return resp
 		if isinstance(r, dict):
+			logging.info('dict')
 			template = r.get('__template__')
+			logging.info(template)
 			if template is None:
 				resp = web.Response(body=json.dumps(r,ensure_ascii=False, default=lambda o : o.__dict__).encode('utf-8'))
 				resp.content_type = 'application/json;charset=utf-8'
 				return resp
 			else:
 				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+				resp.content_type = 'text/html;charset=utf-8'
+				return resp
 		if isinstance(r,int) and t >= 100 and t < 600:
+			logging.info('int.')
 			return web.Response(t)
 		if isinstance(r,tuple) and len(r) == 2:
+			logging.info('tuple.')
 			t,m = r
 			if isinstance(t,int) and t >= 100 and t < 600:
 				return web.Response(t,str(m))
