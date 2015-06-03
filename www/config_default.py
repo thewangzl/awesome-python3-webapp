f9ae3cd0ef31da6f1a0bfd0fb1c7e8bb3bdf6653
thewangzl <thewangzl@gmail.com>
2015-06-03 10:21:36 +0800

    web框架+配置文件

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!
+
+
+'''
+Default Configuration
+'''
+
+configs = {
+	'debug':True,
+	'db':{
+		'host':'127.0.0.1',
+		'port':3306,
+		'user':'thewangzl',
+		'password':'junjun6012',
+		'db':'awesome'
+	},
+	'session':{
+		'secret':'Aswsome'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,10 @@
+#!
+
+'''
+Override configurations
+'''
+
+configs = {
+	'db':'127.0.0.1'
+}
+
